38cad7f9bcecfe5bfd06af60d7e756f08e9f3acf
aliladakhi <115277779+aliladakhi@users.noreply.github.com>
2024-08-26 16:17:51 +0530

    Time: 22 ms (80.09%) | Memory: 49.1 MB (31.30%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -6,10 +6,9 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
                 count++;
-                          max_1=max(max_1,count);
+                max_1=max(max_1,count);
             }
             else if(nums[i]==0){
-                max_1=max(max_1,count);
                 count=0;
             }
         }
